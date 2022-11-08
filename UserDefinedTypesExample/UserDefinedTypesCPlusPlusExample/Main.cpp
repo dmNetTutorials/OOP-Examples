e857e9e2cb06a61fe671fd3f6396b83cf8b07370
dmNetTutorials <d.o.sulyma@npu.edu.ua>
2022-11-08 18:20:47 +0200

    UserDefinedTypes

---
+++ UserDefinedTypesExample/UserDefinedTypesCPlusPlusExample/Main.cpp
@@ -0,0 +1,18 @@
+#include"Main.h"
+
+int main()
+{
+	setlocale(LC_ALL, "");
+
+	
+
+}
+
+// Користувач системи
+struct User
+{
+	// Змінні (поля) описують користувача
+	const char* FirstName = "Дмитро";
+	const char* SecondName = "Сулима";
+
+};
