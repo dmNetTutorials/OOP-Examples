674cf6611cfe727a2b7e4e788df6a55a392eb752
Dmitry <dmitry.sulyma@gmail.com>
2022-11-08 21:49:42 +0200

    Solution Data Types Fix

--- UserDefinedTypesExample/UserDefinedTypesCPlusPlusExample/Main.cpp
+++ UserDefinedTypesExample/UserDefinedTypesCPlusPlusExample/Main.cpp
@@ -4,8 +4,13 @@ int main()
 {
 	setlocale(LC_ALL, "");
 
-	
+	Document doc = Document();
+	// або
+	Document* docPointer = new Document();
 
+	doc.GetAuthor();
+	// або
+	docPointer->GetAuthor();
 }
 
 // Користувач системи
@@ -15,4 +20,45 @@ struct User
 	const char* FirstName = "Дмитро";
 	const char* SecondName = "Сулима";
 
+};
+
+struct User
+{
+	// Ел. пошта користувача
+	char* Email;
+	// Приховані відомості про користувача
+	struct UserMetaData {
+		// Шлях до особистого каталогу
+		char* PersonalFolderPath;
+		// Ідентифікатор користувача
+		char* UserID;
+	};
+};
+
+class Document
+{
+// зазвичай, назви закритих 
+// та захищених елементів починаються 
+// з символу _ (нижня риска)
+private:
+	// Унікальні властивості для
+	// даного виду документа
+
+	// Реєстраційний номер документа
+	char* _registerID;
+protected:
+	// Характерні властивості для 
+	// подібних документів
+
+	// Автор документа
+	char* _author;
+	// Дата створення документа
+	unsigned long int _creationDate;
+public:
+	// Загальнодоступна частина (інтерфейс)
+
+	// Дізнатись хто автор документа
+	char* GetAuthor();
+	// Дізнатись дату створення документа
+	unsigned long int GetCreationDate();
 };
