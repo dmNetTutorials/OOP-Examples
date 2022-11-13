70cc29671b691a92a80ef75dd3d82f8c23786efb
Dmitry <dmitry.sulyma@gmail.com>
2022-11-13 21:48:15 +0200

    Data Types Intro Improvements

--- UserDefinedTypesExample/UserDefinedTypesCPlusPlusExample/Main.cpp
+++ UserDefinedTypesExample/UserDefinedTypesCPlusPlusExample/Main.cpp
@@ -30,6 +30,12 @@ public:
 	// можна зробити архівним
 	mutable bool IsArchived;
 
+	// Чи затверджено документ?
+	bool IsCompleted;
+
+	// Статус документа
+	DocumentStatus Status;
+
 	// Загальнодоступна частина (інтерфейс)
 	// Дізнатись хто автор документа
 	char* GetAuthor()
@@ -77,10 +83,94 @@ public:
 	}
 };
 
+void header(const char* text)
+{
+
+	cout << "Використання " << text << " змiнних" <<
+		" в якостi полiв типу користувача" << endl;
+}
+
+void simpleFieldSample()
+{
+	header("звичайних");
+
+	// Створення екземпляру (instance) 
+	// типу даних користувача
+	UserStorageInfo storageInfo = UserStorageInfo();
+	// Задати к-сть вільної пам'яті
+	// в гігабайтах
+	storageInfo.FreeSpace = 5;
+	// Вивести к-сть вільної пам'яті
+	cout << storageInfo.FreeSpace << endl;
+}
+
+void mutableFieldSample()
+{
+	header("mutable");
+
+	// Створення незмінного 
+	// (затвердженного) документа
+	const Document doc = Document();
+	// Не можна змінювати незмінні 
+	// (const) об'єкти
+	// doc.IsCompleted = false;
+	// mutable-поля можна змінювати завжди
+	doc.IsArchived = true;
+
+}
+
+void bitFieldsSample()
+{
+	Document doc = Document();
+	// Документ є діючим, використання бітового поля
+	doc.Status.Active = true;
+	cout << "Об'єм пам'ятi в байтах зайнятий " 
+		 << "об'єднанням з бiтовими полями:\t"
+		 << sizeof(doc.Status) << endl;
+}
+
+void unionSample()
+{
+	Document doc = Document();
+	// Документ є діючим
+	doc.Status.Active = true;
+	cout << "Об'єм пам'ятi в байтах зайнятий об'єднанням:\t"
+		<< sizeof(doc.Status) << endl;
+
+	//cout << "Адреса першого статусу (Активний):\t" 
+	//	 << &doc.Status.Active << endl;
+	//cout << "Адреса останнього статусу (Невiдомий):\t" 
+	//	 << &doc.Status.Unknown << endl;
+}
+void structSample()
+{
+	AuthorMetaData author = AuthorMetaData();
+	// створення порожнього рядка в 7 символів
+	// 7-й символ - символ завершення рядка '\0'
+	author.FirstName = new char[7] {};
+	// запис тексту в рядок
+	strcpy(author.FirstName, "Дмитро");
+	author.SecondName = new char[7] {};
+	strcpy(author.FirstName, "Сулима");
+	cout << "Об'єм пам'ятi в байтах зайнятий структурою:\t"
+		<< sizeof(author) << endl;
+
+	cout << "Адреса першого поля (FirstName):\t"
+		<< &author.FirstName << endl;
+	cout << "Адреса другого поля (SecondName):\t"
+		<< &author.SecondName << endl;
+}
+
 int main()
 {
-	setlocale(LC_ALL, "");
+	setlocale(LC_ALL, "ukr.utf8");
 
+	//simpleFieldSample();
+	//mutableFieldSample();
+	//unionSample();
+	//structSample();
+	bitFieldsSample();
+	// конкретний документ
 	Document doc = Document();
 	// або
 	Document* docPointer = new Document();
@@ -107,4 +197,3 @@ int main()
 //		char* UserID;
 //	};
 //};
-
--- UserDefinedTypesExample/UserDefinedTypesCPlusPlusExample/Main.h
+++ UserDefinedTypesExample/UserDefinedTypesCPlusPlusExample/Main.h
@@ -1,5 +1,10 @@
 #pragma once
 
+// підтримка старих "небезпечних"
+// функцій мови програмування С
+#define _CRT_SECURE_NO_WARNINGS
+
 #include<iostream>
+#include"IDocument.h"
 
 using namespace std;
